4c7e36372e120d2a6464c33c3f0e59fbdcddad21
avrachimi <avrachimi@hotmail.com>
2020-12-26 19:43:24 +0000

    needs work

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 Simple User Authentication example using Node, Express and JWT.
 
 
-Initially followed this [video](https://www.youtube.com/watch?v=7nafaH9SddU).
+## Resources Used
+- [JWT/Node Youtube Video](https://www.youtube.com/watch?v=7nafaH9SddU).
+- [Guide for using JWT with Node](https://medium.com/better-programming/a-practical-guide-for-jwt-authentication-using-nodejs-and-express-d48369e7e6d4)
--- app.js
+++ app.js
@@ -1,10 +1,59 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 dotenv.config();
+
+// Mock Users for testing
+const users = [
+    {
+        id: 1,
+        username: 'smems',
+        password: '123456'
+    },
+    {
+        id: 2,
+        username: 'admin',
+        password: 'admin'
+    },
+]
+
+// Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/api/users', authenticateToken);
+
+// TOKEN FORMAT
+// Authorization: Bearer <access_token>
+function authenticateToken(req, res, next) {
+    // Get auth header value
+    const bearerHeader = req.headers['authorization'];
+
+    // Check if bearer is undefined
+    if (typeof bearerHeader !== 'undefined') {
+        const bearer = bearerHeader.split(' ');
+        const bearerToken = bearer[1]; // Get token from array
+        req.token = bearerToken; // Set the token
+        next();
+    }
+    else {
+        res.sendStatus(401); // Unauthorized
+    }
+
+    // Verify user
+    jwt.verify(req.token, process.env.TOKEN_SECRET, (err, user) => {
+        if (err) {
+            res.sendStatus(403); // Forbidden
+        }
+        else { // User verified
+            req.user = user;
+            next();
+        }
+    });
+}
 
 // Routes
 // GET
@@ -14,59 +63,43 @@ app.get('/api', (req, res) => {
     });
 });
 
+app.get('/api/users', authenticateToken, (req, res) => {
+    res.json({
+        users: users
+    });
+});
+
 // POST
 app.post('/api/login', (req, res) => {
-    // Mock user
-    const user = {
-        id: 1,
-        username: 'smems',
-        email: 'smems@smems.com'
-    }
+    // Get user data
+    const username = req.body.username;
 
-    // Sign and send token
-    jwt.sign({user}, process.env.TOKEN_SECRET, {expiresIn: 10}, (err, token) => {
-        res.json({
-            token
-        });
-    });
-});
+    const user = users[0]; // Use mock user object for now
 
-app.post('/api/posts', verifyToken, (req, res) => {
-    jwt.verify(req.token, process.env.TOKEN_SECRET, (err, authData) => {
-        if (err) {
-            res.sendStatus(403);
-        }
-        else {
+    if (user) {
+        // Sign and send token
+        jwt.sign({user}, process.env.TOKEN_SECRET, {expiresIn: '1h'}, (err, token) => {
             res.json({
-                message: 'Post',
-                authData
+                token
             });
-        }
+        });
+    } else {
+        res.json({
+            error: 'Username or Password incorrect.'
+        });
+    }
+
+
+});
+
+app.post('/api/posts', authenticateToken, (req, res) => {
+    console.log(req.user.username);
+    res.json({
+        message: 'Post',
+        user: req.user
     });
 });
 
-// Middleware
-
-// TOKEN FORMAT
-// Authorization: Bearer <access_token>
-function verifyToken(req, res, next) {
-    // Get auth header value
-    const bearerHeader = req.headers['authorization'];
-
-    // Check if bearer is undefined
-    if (typeof bearerHeader !== 'undefined') {
-        const bearer = bearerHeader.split(' ');
-        // Get token from array
-        const bearerToken = bearer[1];
-        // Set the token
-        req.token = bearerToken;
-        next();
-    }
-    else {
-        // Forbidden
-        res.sendStatus(403);
-    }
-}
 
 const port = process.env.PORT | 3000;
-app.listen(port, () => console.log(`Server started on PORT ${port}`));
+app.listen(port, () => console.log(`Server running on PORT ${port}...`));
